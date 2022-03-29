37d041a8fb15f9bba0fae89256cca78cc880b995
Silas Okpugie <silasokpugie@cpe-172-100-0-53.twcny.res.rr.com>
2022-03-29 17:56:34 +0100

    new changes

--- src/config/config.cjs
+++ src/config/config.cjs
@@ -18,10 +18,6 @@ module.exports = {
     dialect: process.env.DIALECT,
   },
   production: {
-    username: process.env.USERNAME,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    host: process.env.HOST,
-    dialect: process.env.DIALECT,
+    database: process.env.DATABASE_URL,
   },
 };
