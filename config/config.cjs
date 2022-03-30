505c36acf4658a75982f37f31f41242480a44998
Silas Okpugie <silasokpugie@cpe-172-100-0-106.twcny.res.rr.com>
2022-03-30 13:13:56 +0100

    chznge

--- config/config.cjs
+++ config/config.cjs
@@ -17,4 +17,11 @@ module.exports = {
     host: process.env.HOST,
     dialect: process.env.DIALECT,
   },
+  production: {
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
+    dialect: process.env.DIALECT,
+  },
 };
