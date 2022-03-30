385b365f1f5ff582e613ba6c54404d30556af3cc
Silas Okpugie <silasokpugie@cpe-172-100-0-106.twcny.res.rr.com>
2022-03-30 15:16:50 +0100

    chnages

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const sequelizeConnection = () => new Sequelize('postgres://postgres:silas2001@localhost:5432/E-library', { dialect: process.env.DIALECT});
+const sequelizeConnection = () => new Sequelize('postgres://postgres:silas2001@localhost:5432/E-library');
 
 export const db = {
   Sequelize,
