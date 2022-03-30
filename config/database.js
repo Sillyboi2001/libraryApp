e66a58b482ac65ddfc1d2633e21f75159decdcad
Silas Okpugie <silasokpugie@cpe-172-100-0-106.twcny.res.rr.com>
2022-03-30 12:52:19 +0100

    changes

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const sequelizeConnection = () => new Sequelize(process.env.DATABASE_URL1);
+const sequelizeConnection = () => new Sequelize(process.env.DATABASE_URL1, { dialect: process.env.DIALECT});
 
 export const db = {
   Sequelize,
