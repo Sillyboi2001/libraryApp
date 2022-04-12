d7e6e8c842aeda494a3476bc328df4fef3136237
Silas Okpugie <silasokpugie@cpe-172-100-0-115.twcny.res.rr.com>
2022-04-12 11:26:13 +0100

    circleci setup

--- config/config.cjs
+++ config/config.cjs
@@ -11,12 +11,14 @@ module.exports = {
     dialect: process.env.DIALECT,
   },
   test: {
-    username: process.env.USERNAME,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    host: process.env.HOST,
-    dialect: process.env.DIALECT,
-    port: process.env.PG_PORT,
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   production: {
     use_env_variable: 'DATABASE_URL',
