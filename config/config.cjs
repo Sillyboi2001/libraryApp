dac64af41129508452ee2ce2fb5aafe09b0c7c50
Silas Okpugie <silasokpugie@cpe-172-100-0-134.twcny.res.rr.com>
2022-04-13 10:46:09 +0100

    new change

--- config/config.cjs
+++ config/config.cjs
@@ -23,6 +23,8 @@ module.exports = {
   production: {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
+    protocol: 'postgres',
+    ssl: true,
     dialectOptions: {
       ssl: {
         require: true,
